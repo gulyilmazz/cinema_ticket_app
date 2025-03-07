1952df36175ac0fd7b3ea347579303cf88ca6f6b
Gul-yilmaz <yilmazlutfiyegul@gmail.com>
2025-03-07 08:26:06 +0300

    login ekranı yapıldı

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,82 +1,207 @@
 import 'package:flutter/material.dart';
 import 'register_screen.dart';
 import 'home_film_listesi.dart';
-import '../utils/renkler.dart'; // Renkler sınıfını içe aktar
+import '../utils/renkler.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _passwordVisible = false;
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Renkler.arkaPlanRengi, // Arka plan rengini ayarla
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              style: TextStyle(
-                color: Renkler.metinRengi,
-              ), // Metin rengini ayarla
-              decoration: InputDecoration(
-                labelText: 'E-posta veya Telefon',
-                labelStyle: TextStyle(color: Renkler.metinRengi),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Renkler.metinRengi),
+    return GestureDetector(
+      onTap: () {
+        WidgetsBinding.instance.focusManager.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        extendBodyBehindAppBar: true,
+        extendBody: true,
+        backgroundColor: Renkler.arkaPlanRengi,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom:
+                    MediaQuery.of(context).viewInsets.bottom +
+                    MediaQuery.of(context).padding.bottom,
+              ),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: MediaQuery.of(context).size.height,
                 ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Renkler.butonRengi),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color.fromARGB(255, 8, 12, 9),
+                        const Color.fromARGB(255, 90, 85, 134),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        // GIF Alanı
+                        Image.asset(
+                          'lib/assets/images/video.gif', // GIF dosyanızın yolunu buraya ekleyin
+                          height: 80, // GIF'in yüksekliğini ayarlayın
+                        ),
+                        SizedBox(height: 40),
+                        Text(
+                          'SAHNE',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(255, 247, 247, 247),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Film Dünyasına Giriş Yapın',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                        SizedBox(height: 30),
+                        TextField(
+                          controller: _emailController,
+                          focusNode: _emailFocusNode,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Username',
+                            labelStyle: TextStyle(
+                              color: const Color.fromARGB(255, 158, 158, 158),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: const Color.fromARGB(255, 3, 12, 19),
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: Icon(Icons.person, color: Colors.grey),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        TextField(
+                          controller: _passwordController,
+                          focusNode: _passwordFocusNode,
+                          obscureText: !_passwordVisible,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.grey),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: const Color.fromARGB(255, 203, 217, 229),
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.grey,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => FilmListesi(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              42,
+                              43,
+                              38,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 40,
+                              vertical: 15,
+                            ),
+                            child: Text(
+                              'Giriş >',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RegisterScreen(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            "Hesabın yok mu? Kayıt ol",
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 199, 195, 195),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
-            TextField(
-              controller: _passwordController,
-              style: TextStyle(
-                color: Renkler.metinRengi,
-              ), // Metin rengini ayarla
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Şifre',
-                labelStyle: TextStyle(color: Renkler.metinRengi),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Renkler.metinRengi),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Renkler.butonRengi),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Giriş doğrulama işlemleri
-                // Başarılı giriş sonrası film listesi ekranına yönlendirme
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => FilmListesi()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Renkler.butonRengi, // Buton rengini ayarla
-              ),
-              child: Text('Giriş', style: TextStyle(color: Renkler.metinRengi)),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterScreen()),
-                );
-              },
-              child: Text(
-                "Hesabın yok mu? Kayıt ol",
-                style: TextStyle(color: Renkler.metinRengi),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
