35c03d0f0796626d3f396da0295a2bb9dc18e59f
Gül Yılmaz <yilmazlutfiyegul@gmail.com>
2025-03-25 09:57:04 +0300

    alt buton kaybolma engeli(indexedStack)

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -17,38 +17,28 @@ class _FilmTryState extends State<FilmTry> {
   int _currentPage = 0;
   PageController _pageController = PageController();
 
+  final List<Widget> _pages = [
+    FavorilerSayfasi(),
+    FiltrelemeSecenekleriSayfasi(),
+    BiletlerSayfasi(),
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-
-    if (index == 0) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => FavorilerSayfasi()),
-      );
-    } else if (index == 1) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => FiltrelemeSecenekleriSayfasi()),
-      );
-    } else if (index == 2) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => BiletlerSayfasi()),
-      );
-    }
   }
 
   @override
   Widget build(BuildContext context) {
+    //Ana sayfa tasarımı
     return Scaffold(
       appBar: AppBar(
         title: Text('Vizyondaki Filmler'),
         backgroundColor: const Color.fromARGB(255, 31, 177, 187),
       ),
+      //Drawer içindeki menü
       drawer: Drawer(
-        // Drawer ekleme
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
@@ -65,87 +55,113 @@ class _FilmTryState extends State<FilmTry> {
               leading: Icon(Icons.home),
               title: Text('Ana Sayfa'),
               onTap: () {
-                // Ana sayfaya yönlendirme veya başka bir işlem
-                Navigator.pop(context); // Drawer'ı kapat
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => AnaSayfa()));
+                Navigator.pop(context);
               },
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Ayarlar'),
               onTap: () {
-                // Ayarlar sayfasına yönlendirme veya başka bir işlem
-                Navigator.pop(context); // Drawer'ı kapat
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => AyarlarSayfasi()));
+                Navigator.pop(context);
               },
             ),
-            // İhtiyacınıza göre daha fazla ListTile ekleyebilirsiniz
           ],
         ),
       ),
-      body: Column(
+
+      //Ana sayfa icerigi
+      body: IndexedStack(
+        //alt buton kaybolması için kullanılan widget
+
+        /* acıklma: IndexedStack, birden fazla widget'ı üst üste yerleştirir ve
+        _selectedIndex'e göre sadece birini görüntüler.
+        Bu, alt navigasyon çubuğundaki sayfa geçişlerini yönetir.*/
+        index: _selectedIndex,
         children: <Widget>[
-          Container(
-            height: 200,
-            width: double.infinity,
-            child: PageView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              controller: _pageController,
-              itemCount: Film.filmler.length,
-              itemBuilder: (context, index) {
-                return KaydirmaliFilmKarti(film: Film.filmler[index]);
-              },
-              onPageChanged: (int page) {
-                setState(() {
-                  _currentPage = page;
-                });
-              },
-            ),
-          ),
-          DotsIndicator(
-            dotsCount: Film.filmler.length,
-            position: _currentPage.toInt(),
-            decorator: DotsDecorator(
-              color: const Color.fromARGB(255, 111, 145, 148),
-              activeColor: const Color.fromARGB(255, 31, 177, 187),
-            ),
-            onTap: (position) {
-              _pageController.animateToPage(
-                position,
-                duration: Duration(milliseconds: 300),
-                curve: Curves.ease,
-              );
-            },
-          ),
-          Expanded(
-            child: Container(
-              color: const Color.fromARGB(255, 0, 0, 0),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.7,
+          Column(
+            children: <Widget>[
+              Container(
+                //Kaydırılan filmlerin bulunduğu alan
+                height: 200,
+                width: double.infinity,
+                child: PageView.builder(
+                  //PageView, kaydırılabilir bir widget oluşturur
+                  physics:
+                      NeverScrollableScrollPhysics(), //Kaydırma özelliğini kapatır
+                  controller: _pageController,
+                  itemCount:
+                      Film
+                          .filmler
+                          .length, //Film sayısı kadar kaydırılabilir alan oluşturur
+                  itemBuilder: (context, index) {
+                    //Kaydırılan filmler
+                    return KaydirmaliFilmKarti(film: Film.filmler[index]);
+                  },
+                  onPageChanged: (int page) {
+                    setState(() {
+                      //Kaydırılan filmler arasında geçiş yaparken sayfa numarasını günceller
+
+                      _currentPage =
+                          page; //_currentPage, sayfa numarasını tutar
+                    });
+                  },
                 ),
-                itemCount: Film.filmler.length,
-                itemBuilder: (context, index) {
-                  return FilmKarti(film: Film.filmler[index]);
+              ),
+
+              //Kaydırılan filmlerin altında bulunan noktalar
+              DotsIndicator(
+                dotsCount:
+                    Film.filmler.length, //Film sayısı kadar nokta oluşturur
+                position: _currentPage.toInt(), //Noktaların konumunu günceller
+                decorator: DotsDecorator(
+                  //Noktaların tasarımını belirler
+                  color: const Color.fromARGB(255, 111, 145, 148),
+                  activeColor: const Color.fromARGB(255, 31, 177, 187),
+                ),
+                onTap: (position) {
+                  _pageController.animateToPage(
+                    //Noktalara tıklandığında sayfayı değiştirir
+                    position,
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.ease, //Sayfa geçişini yumuşatır
+                  );
                 },
               ),
-            ),
+              Expanded(
+                //Film listesini ızgara şeklinde gösteren GridView.builder için boş alanı doldurur.
+                child: Container(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      //Izgara görünümünü belirler
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemCount: Film.filmler.length,
+                    itemBuilder: (context, index) {
+                      return FilmKarti(film: Film.filmler[index]);
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
+          ..._pages,
         ],
       ),
+      //Alt navigasyon çubuğu
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border_outlined),
-            label: '',
-          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.movie_filter_sharp),
             label: '',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.confirmation_num_outlined),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_outlined),
             label: '',
           ),
         ],
