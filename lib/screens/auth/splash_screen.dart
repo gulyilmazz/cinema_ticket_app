688c191be5536adc0a794a659ec3fb08ae76006e
Gül Yılmaz <yilmazlutfiyegul@gmail.com>
2025-05-30 10:18:52 +0300

    .

--- lib/screens/auth/splash_screen.dart
+++ lib/screens/auth/splash_screen.dart
@@ -14,7 +14,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   // Splash ekranının kaç saniye görüneceği
-  static const int splashDuration = 3;
+  static const int splashDuration = 5;
   late String token;
 
   void initToken() async {
